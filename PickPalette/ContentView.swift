04fff0da13b0e67b0cbd94bcc01f387e690e4388
sak0a <laurinfrank03@gmail.com>
2026-02-06 02:25:34 +0100

    Initial Commit

---
+++ PickPalette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickPalette
+//
+//  Created by Laurin Frank on 06.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickPalette/PickPaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickPaletteApp.swift
+//  PickPalette
+//
+//  Created by Laurin Frank on 06.02.26.
+//
+
+import SwiftUI
+
+@main
+struct PickPaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
